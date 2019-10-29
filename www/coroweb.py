2f4a1601a3b65df44f97554da94b7e53f08ca368
Evan_Watson <Evan_watson@163.com>
2019-10-29 08:50:46 +0800

    finish day4 finish apis.py solve the Error problem add jupyter notebook test

--- www/coroweb.py
+++ www/coroweb.py
@@ -3,3 +3,32 @@
 import asyncio, os, inspect, logging, functools
 from urllib import parse
 from aiohttp import web
+from www.apis import APIError
+
+def get(path):
+    def decrator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+    return decrator
+
+def post(path):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'POST'
+        wrapper.__route__ = path
+        return wrapper
+    return decorator
+
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
